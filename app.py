135faeb32f7809b722db1feca22e91e996c6d4e5
SrinathBala <92857200+SrinathBala@users.noreply.github.com>
2021-12-09 14:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Love you Brindha'
+    return 'hello World From Srinath'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
